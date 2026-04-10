7409d73b2393e6e769b563f2995203393d0af2f2
Prashanth Shivananjappa <chotur15@gmail.com>
2026-04-10 08:56:40 +0530

    class vs function

--- hello.py
+++ hello.py
@@ -1,5 +1,24 @@
 
-print("hello world")
-print("this is my second commit")
+# print("hello world")
+# print("this is my second commit")
 
 
+
+class Dog:
+    species = "Canis familiaris"
+    name = "Rex"
+    age = 3
+
+dog = Dog()
+print(dog.name)
+print(dog.age)
+print(dog.species)
+
+def bark(name):
+    return f"{name} says: woof!"
+
+def describe(name, age):
+    return f"{name} is {age} years old."
+
+print(bark("Rex"))
+print(describe("Rex", 3))
